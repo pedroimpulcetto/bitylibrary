93e3db0ebd0dd0eb34e2e92a041b24894c33d741
root <root@impulcetto.localdomain>
2020-09-30 18:30:37 -0300

    Pronto para deploy

--- library/settings.py
+++ library/settings.py
@@ -77,6 +77,13 @@ WSGI_APPLICATION = 'library.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
