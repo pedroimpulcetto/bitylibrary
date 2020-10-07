07ea9499f8f3ad115a5cf04258700417bd9542cd
root <root@impulcetto.localdomain>
2020-10-07 19:21:06 -0300

    AmazonS3

--- library/settings.py
+++ library/settings.py
@@ -128,3 +128,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 django_heroku.settings(locals())
+
+AWS_ACCESS_KEY_ID = 'AKIAVYCMGDVV7BQKIC3H'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'bity-library-pedroimpulcetto'
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
