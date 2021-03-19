e17c37c08eba492d47129f52af556d2782454389
Pedro Impulcetto <pedro.impulcetto@gmail.com>
2021-03-19 18:58:58 -0300

    fix

--- library/settings.py
+++ library/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = '_eb*!x78vr!@tzgicr56wp5oikbo=hh7cty5+4v9x17vhilp(6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
